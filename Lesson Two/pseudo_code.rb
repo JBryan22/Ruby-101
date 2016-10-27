0d2477d7c5e9ad1d5a5f6caee3eb248526441967
JBryan22 <jesse.bryan22@gmail.com>
2016-10-27 09:54:09 -0700

    Calc updated

---
+++ Lesson Two/pseudo_code.rb
@@ -0,0 +1,37 @@
+START
+
+PRINT welcoming message and ask for two numbers
+
+GET number 1
+GET number 2
+
+add number1 and number2 together
+
+PRINT result
+
+END
+
+START
+
+define the method with one parameter that is an array
+
+  combine the array into one string
+  return the new concat string
+
+END
+
+START
+
+define method(array of ints)
+
+  return if array is nil
+  SET newarray = nil
+
+  while iterator < length of array
+    newarray += array[iterator]
+    iterator + 2
+  end
+
+  return newarray
+
+END
